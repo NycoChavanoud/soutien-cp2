e4817a7b4d89b2c41fef4d1297f8007bababa91d
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-05-04 23:59:38 +0200

    add filter stark on list

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,29 +4,33 @@ import CharactersCard from "../components/CharactersCard";
 
 const Home = () => {
   const [charactersList, setCharctersList] = useState([]);
+  const [onlyStark, setOnlyStark] = useState(false);
   useEffect(() => {
     axios
       .get("https://thronesapi.com/api/v2/Characters")
       .then((res) => setCharctersList(res.data));
   }, []);
 
-  if (!charactersList) {
-    return <span>LOADING...</span>;
-  }
-
   return (
     <>
-      {charactersList.map((character, index) => {
-        return (
-          <CharactersCard
-            key={index}
-            id={character.id}
-            fullName={character.fullName}
-            title={character.title}
-            family={character.family}
-          />
-        );
-      })}
+      <button onClick={() => setOnlyStark(!onlyStark)}>FILTRE STARK</button>
+      {charactersList
+        .filter((character) =>
+          character.family === "House Stark" || character.family === "Stark"
+            ? onlyStark
+            : !onlyStark
+        )
+        .map((character, index) => {
+          return (
+            <CharactersCard
+              key={index}
+              id={character.id}
+              fullName={character.fullName}
+              title={character.title}
+              family={character.family}
+            />
+          );
+        })}
     </>
   );
 };
