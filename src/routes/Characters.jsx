03c1603f34cc3b461778368313768a541886228f
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-05-04 23:35:03 +0200

    add details on characters page details

--- src/routes/Characters.jsx
+++ src/routes/Characters.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Characters = () => {
-  return <div>Characters</div>;
+  const { id } = useParams();
+  const [charactersDetails, setCharactersDetails] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://thronesapi.com/api/v2/Characters/${id}`)
+      .then((res) => setCharactersDetails(res.data));
+  }, [id]);
+
+  console.log(charactersDetails);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "50px",
+      }}
+    >
+      <div>{charactersDetails.firstName}</div>
+      <div>{charactersDetails.lastName}</div>
+      <img src={charactersDetails.imageUrl} alt={charactersDetails.fullName} />
+      <div>{charactersDetails.title}</div>
+      <div>{charactersDetails.family}</div>
+    </div>
+  );
 };
 
 export default Characters;
